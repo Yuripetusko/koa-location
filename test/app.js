fd34e4c665366b90a6ed72c82e92334294259567
Dragan <iliya5376@mail.com>
2017-08-15 23:22:35 +0300

    feature(lib/index.js) add free urls

--- test/app.js
+++ test/app.js
@@ -2,20 +2,20 @@ var Koa = require('koa');
 var app = new Koa();
 //app.proxy = true;
 
-var middleware = require('./lib');
+var middleware = require('./../lib');
 
 app.use(middleware({
-	autoDetect: true
+	autoDetect: true,
+	freeUrls: ['/', '/cekavo','/hello']
 }));
 
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
 	ctx.body = 'Ce Kavo';
 	//await ctx.request.detect();
-	//console.log(ctx.request.headers['x-forwarded-for']);
 	console.log(ctx.request.locationData);
 });
 
 var port = 8000;
-app.listen(port, ()=> {
+app.listen(port, () => {
 	console.log('Сервер работает на порту ' + port);
 });
